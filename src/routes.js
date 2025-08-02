85f03022d9a41c22a89c813ac92a1e4b65df77ff
Arek <arkadiuszkorzeniowski97@gmail.com>
2025-08-02 12:54:06 +0200

    next commit

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 const routes = [
-    { path: '/', component: () => import('./pages/StronaGlowna.vue') }, // <- dodane
+    { path: '/', component: () => import('./pages/StronaGlowna.vue') },
     { path: '/Omnie', component: () => import('./pages/Omnie.vue') },
     { path: '/Kontakt', component: () => import('./pages/Kontakt.vue') },
 ]
