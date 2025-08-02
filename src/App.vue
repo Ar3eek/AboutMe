53a8d74cdfa09b39737d472a23ad1ab0b2b76ce1
Arek <arkadiuszkorzeniowski97@gmail.com>
2025-08-02 13:31:04 +0200

    next commit

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-  <ul class="menu">
+  <ul>
     <li><router-link to="/"></router-link></li>
     <li><router-link to="/Omnie"></router-link></li>
     <li><router-link to="/Kontakt"></router-link></li>
   </ul>
-  <router-view></router-view>
+  <router-view/>
 </template>
 
 <style scoped>
