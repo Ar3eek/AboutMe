f21911f2c7051b49876fdb636bdd431ba5ab9dd5
Arek <arkadiuszkorzeniowski97@gmail.com>
2025-08-02 13:12:46 +0200

    next commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // ← to sprawia, że ścieżki do zasobów będą działać z dowolnego folderu
   plugins: [vue()],
 })
